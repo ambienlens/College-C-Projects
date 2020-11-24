11a7d26d72a5e6a7caccadee3cf44e1a6fce7315
ambientlensation <debayan.majumder121@gmail.com>
2020-11-24 11:22:26 +0530

    New Files Added

---
+++ Assignment 8/Q1.cpp
@@ -0,0 +1,49 @@
+//Debayan Majumder
+//Write a program to sort a list of elements using bubble sort algorithm and also implement modified bubble sort.
+
+#include <stdio.h>
+#define MAX 10
+
+void inputArray(int array[], int n)
+{
+    printf("Enter the array: \n");
+    for(int i=0; i<n; i++)
+        scanf("%d", &array[i]);
+}
+
+void displayArray(int array[], int n)
+{
+    for(int i=0; i<n; i++)
+        printf("%d\n", array[i]);
+}
+
+void bubblesort(int array[], int n)
+{
+    int temp;
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if(array[j] > array[j+1])
+            {
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    printf("Array after sorting: \n");
+    displayArray(array, n);
+}
+
+int main()
+{
+    int array[MAX], n;
+    printf("Enter the no. of elements: ");
+    scanf("%d", &n);
+    inputArray(array, n);
+    printf("Original Array: \n");
+    displayArray(array, n);
+    bubblesort(array, n);
+    return 0;
+}
---
+++ Assignment 8/README.md
@@ -0,0 +1,6 @@
+# Write C programs to perform following operations using functions:
+
+- Write a program to sort a list of elements using bubble sort algorithm and also
+implement modified bubble sort.
+
+- Write a program to sort a list of elements using selection sort algorithm.
