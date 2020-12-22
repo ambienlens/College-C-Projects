0bdeb017d709d9fc8e0fbe2dc714c3c71a0d9733
ambientlensation <debayan.majumder121@gmail.com>
2020-12-22 11:31:26 +0530

    Updated Files

--- Assignment 13/Q1.cpp
+++ Assignment 13/Q1.cpp
@@ -1,123 +1,136 @@
-//Debayan Majumder
-//Stack using LinkedList.
 
-#include <stdio.h>  
-#include <stdlib.h>  
-void push();  
-void pop();  
-void display();  
+//C Program to Implement a Stack using Linked List
 
-struct node   
-{  
-    int val;
-    struct node *next;  
-};  
-struct node *head;  
-  
-void main ()  
-{  
-    int choice=0;     
-    printf("\n*********Stack operations using linked list*********\n");  
-    printf("\n----------------------------------------------\n");  
-    while(choice != 4)  
-    {  
-        printf("\nChose one from the below options...\n");  
-        printf("\n1.Push\n2.Pop\n3.Show\n4.Exit");  
-        printf("\nEnter your choice \n");        
-        scanf("%d",&choice);  
-        switch(choice)  
-        {  
-            case 1:  
-            {   
-                push();  
-                break;  
-            }  
-            case 2:  
-            {  
-                pop();  
-                break;  
-            }  
-            case 3:  
-            {  
-                display();  
-                break;  
-            }  
-            case 4:   
-            {  
-                printf("Exiting....");  
-                break;   
-            }  
-            default:  
-            {  
-                printf("Please Enter valid choice ");  
-            }   
-    };  
-}  
-}  
-void push ()  
-{  
-    int val;  
-    struct node *ptr = (struct node*)malloc(sizeof(struct node));   
-    if(ptr == NULL)  
-    {  
-        printf("not able to push the element");   
-    }  
-    else   
-    {  
-        printf("Enter the value");  
-        scanf("%d",&val);  
-        if(head==NULL)  
-        {         
-            ptr->val = val;  
-            ptr -> next = NULL;  
-            head=ptr;  
-        }   
-        else   
-        {  
-            ptr->val = val;  
-            ptr->next = head;  
-            head=ptr;  
-               
-        }  
-        printf("Item pushed");  
-          
-    }  
-}  
-  
-void pop()  
-{  
-    int item;  
-    struct node *ptr;  
-    if (head == NULL)  
-    {  
-        printf("Underflow");  
-    }  
-    else  
-    {  
-        item = head->val;  
-        ptr = head;  
-        head = head->next;  
-        free(ptr);  
-        printf("Item popped");  
-          
-    }  
-}  
-void display()  
-{  
-    int i;  
-    struct node *ptr;  
-    ptr=head;  
-    if(ptr == NULL)  
-    {  
-        printf("Stack is empty\n");  
-    }  
-    else  
-    {  
-        printf("Printing Stack elements \n");  
-        while(ptr!=NULL)  
-        {  
-            printf("%d\n",ptr->val);  
-            ptr = ptr->next;  
-        }  
-    }  
-}  
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct node
+{
+    int info;
+    struct node *ptr;
+}*top,*top1,*temp;
+int count = 0;
+
+/* Create empty stack */
+void create()
+{
+    top = NULL;
+}
+ 
+/* Count stack elements */
+void stack_count()
+{
+    printf("\nNo. of elements in stack : %d", count);
+}
+ 
+/* Push data into stack */
+void push(int data)
+{
+    if (top == NULL)
+    {
+        top =(struct node *)malloc(1*sizeof(struct node));
+        top->ptr = NULL;
+        top->info = data;
+    }
+    else
+    {
+        temp =(struct node *)malloc(1*sizeof(struct node));
+        temp->ptr = top;
+        temp->info = data;
+        top = temp;
+    }
+    count++;
+}
+ 
+/* Display stack elements */
+void display()
+{
+    top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("Stack is empty");
+        return;
+    }
+ 
+    while (top1 != NULL)
+    {
+        printf("%d ", top1->info);
+        top1 = top1->ptr;
+    }
+ }
+ 
+/* Pop Operation on stack */
+void pop()
+{
+    top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("\nError : Trying to pop from empty stack");
+        return;
+    }
+    else
+        top1 = top1->ptr;
+    printf("\nPopped value : %d", top->info);
+    free(top);
+    top = top1;
+    count--;
+}
+ 
+/* Return top element */
+int topelement()
+{
+    return(top->info);
+}
+
+void main()
+{
+    int no, ch, e;
+    create();
+ 
+    while (1)
+    {
+        printf("\n1 - Push");
+        printf("\n2 - Pop");
+        printf("\n3 - Top");
+        printf("\n4 - Stack Count");
+        printf("\n5 - Dipslay");
+        printf("\n6 - Exit");
+
+        printf("\nEnter choice : ");
+        scanf("%d", &ch);
+ 
+        switch (ch)
+        {
+        case 1:
+            printf("Enter data : ");
+            scanf("%d", &no);
+            push(no);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            if (top == NULL)
+                printf("No elements in stack");
+            else
+            {
+                e = topelement();
+                printf("\nTop element : %d", e);
+            }
+            break;
+        case 4:
+            stack_count();
+            break;
+        case 5:
+            display();
+            break;
+        case 6:
+            exit(0);
+        default :
+            printf(" Wrong choice, Please enter correct choice  ");
+            break;
+        }
+    }
+}
